830c096a8cde1d4c91761e08a908906f8b0f1c7f
Brett Turing <bjsherman80@gmail.com>
2021-05-18 15:37:54 -0600

    create bot placement with column and row attributes

--- save_princess/board.rb
+++ save_princess/board.rb
@@ -1,8 +1,20 @@
 class Board 
     attr_reader :board,
-                :board_size
+                :board_size,
+                :bot_column,
+                :bot_row
     def initialize(board_size, board)
-        @board_size = board_size
-        @board = board
+      @board_size = board_size
+      @board = board
+      @bot_column = find_bot_column(board_size)
+      @bot_row = find_bot_row(board_size)
+    end
+
+    def find_bot_column(board_size)
+     (board_size - 1) / 2
+    end
+
+    def find_bot_row(board_size)
+     (board_size - 1) / 2
     end
 end 
