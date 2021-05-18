9183bae153254eea829f63c7f7d74faf7c2690b9
Brett Turing <bjsherman80@gmail.com>
2021-05-18 15:46:48 -0600

    refactor bot placement to be arrray with x/y variable

--- save_princess/board.rb
+++ save_princess/board.rb
@@ -1,20 +1,14 @@
 class Board 
     attr_reader :board,
                 :board_size,
-                :bot_column,
-                :bot_row
+                :bot_placement
     def initialize(board_size, board)
       @board_size = board_size
       @board = board
-      @bot_column = find_bot_column(board_size)
-      @bot_row = find_bot_row(board_size)
+      @bot_placement = find_bot_placement(board_size)
     end
 
-    def find_bot_column(board_size)
-     (board_size - 1) / 2
-    end
-
-    def find_bot_row(board_size)
-     (board_size - 1) / 2
+    def find_bot_placement(board_size)
+      [(board_size - 1) / 2, (board_size - 1) / 2]
     end
 end 
