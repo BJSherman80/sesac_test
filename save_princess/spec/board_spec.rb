ef860408e859ae9e0805171fc27103b96a815313
Brett Turing <bjsherman80@gmail.com>
2021-05-18 15:37:08 -0600

    create place bot test

--- save_princess/spec/board_spec.rb
+++ save_princess/spec/board_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Board do
      expect(board.board_size).to eq(3)
      expect(board.board).to eq(["---", "-m-", "p--"])
     end
+
+    # now I need to place the bot in the center of the board with simple math to find middle of odd number
+    
+    it "places bot in the middle of the board" do 
+        board = Board.new(3, ["---", "-m-", "p--"])
+        board_1 = Board.new(9, ["---", "-m-", "p--"])
+        expect(board.bot_column).to eq(1)
+        expect(board.bot_row).to eq(1)
+        expect(board_1.bot_column).to eq(4)
+        expect(board_1.bot_row).to eq(4)
+    end 
+
 end 
