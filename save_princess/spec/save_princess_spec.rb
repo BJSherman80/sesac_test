9ea6a5ab0ae03db8f63db5ab07a5842e2ce4eae1
Brett Turing <bjsherman80@gmail.com>
2021-05-19 11:56:56 -0600

    create tests to find difference in placement and path to princess

--- save_princess/spec/save_princess_spec.rb
+++ save_princess/spec/save_princess_spec.rb
@@ -10,4 +10,31 @@ RSpec.describe SavePrincess do
     expect(game.bot_location).to eq([1,1])
     expect(game.princess_location).to eq([2,0])
   end
+
+  it 'can find placement differences' do 
+    game = SavePrincess.new(3, ["---", "-m-", "p--"])
+
+    expect(game.row_difference).to eq(1)
+    expect(game.column_difference).to eq(-1)
+  end 
+
+  it 'can find path to princess' do 
+    game = SavePrincess.new(3, ["---", "-m-", "p--"])
+    expect(game.path_to_princess).to eq(["DOWN", "LEFT"])
+  end 
+
+  it 'can find path to princess' do 
+    game = SavePrincess.new(3, ["p--", "-m-", "---"])
+    expect(game.path_to_princess).to eq(["UP", "LEFT"])
+  end 
+
+  it 'can find path to princess' do 
+    game = SavePrincess.new(3, ["---", "-m-", "--p"])
+    expect(game.path_to_princess).to eq(["DOWN", "RIGHT"])
+  end 
+
+  it 'can find path to princess' do 
+    game_1 = SavePrincess.new(5, ["-----", "-----", "--m--", "-----", "p----"])
+    expect(game_1.path_to_princess).to eq(["DOWN", "DOWN", "LEFT", "LEFT"])
+  end 
 end
