48c33f23d28bcf44611d2e5c896847290c1d78e4
Brett Turing <bjsherman80@gmail.com>
2021-05-20 11:05:10 -0600

    edit test name

--- save_princess/spec/save_princess_spec.rb
+++ save_princess/spec/save_princess_spec.rb
@@ -4,7 +4,7 @@ require './board'
 require './save_princess'
 
 RSpec.describe SavePrincess do 
-  it 'should have attributes and instance of a board' do
+  it 'should have instance of a board, mario and princess' do
     game = SavePrincess.new(3, ["---", "-m-", "p--"])
  
     expect(game.board).to be_a Board 
