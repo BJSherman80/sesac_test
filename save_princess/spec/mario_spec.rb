bbd0cd7597f7c590ca49036e2d5d166a377749a4
Brett Turing <bjsherman80@gmail.com>
2021-05-19 21:45:01 -0600

    create mario class for scalability

---
+++ save_princess/spec/mario_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require 'pry'
+require './board'
+require './mario'
+
+RSpec.describe Mario do 
+
+    it 'exists' do 
+      board = Board.new(3, ["---", "-m-", "p--"])
+      mario = Mario.new(board)
+      expect(mario).to be_an(Mario)
+    end 
+
+    it 'has correct mario column and row cords' do 
+        board = Board.new(3, ["---", "-m-", "p--"])
+        mario = Mario.new(board)
+
+        expect(mario.column).to eq(1)
+        expect(mario.row).to eq(1)
+    end 
+
+end
