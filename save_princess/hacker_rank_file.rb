abc51462cb63f1970ba01dfdf50562e3b4fe1747
Brett Turing <bjsherman80@gmail.com>
2021-05-20 08:25:58 -0600

    update file to account for mario and princess objects

--- save_princess/hacker_rank_file.rb
+++ save_princess/hacker_rank_file.rb
@@ -1,53 +1,92 @@
 class Board 
-    attr_reader :board,
-                :board_size,
-                :bot_placement,
-                :princess_placement
-    def initialize(board_size, board)
-      @board_size = board_size
-      @board = board
-      @bot_placement = find_bot_placement
-      @princess_placement = find_princess_placement
-    end
+  attr_reader :board,
+              :board_size,
+              :bot_placement,
+              :princess_placement
+  def initialize(board_size, board)
+    @board_size = board_size
+    @board = board
+    @bot_placement = find_bot_placement
+    @princess_placement = find_princess_placement
+  end
 
-    def find_bot_placement
-      [(@board_size - 1) / 2, (@board_size - 1) / 2]
-    end
 
-    def find_princess_placement
-      princess_xy = []
-      corner_positions = [0,0],
-                         [@board_size - 1,0],
-                         [0,@board_size - 1],
-                         [@board_size - 1,@board_size - 1]
-        corner_positions.each do |position|
-          princess_xy << position if @board[position[0]][position[1]] == 'p'
-         end
-      princess_xy.flatten
-    end
+  def find_bot_placement
+    [(@board_size - 1) / 2, (@board_size - 1) / 2]
+  end
+
+  def find_princess_placement
+    princess_xy = []
+    corner_positions = [0,0],
+                       [@board_size - 1,0],
+                       [0,@board_size - 1],
+                       [@board_size - 1,@board_size - 1]
+      corner_positions.each do |position|
+        princess_xy << position if @board[position[0]][position[1]] == 'p'
+       end
+    princess_xy.flatten
+  end
+end 
+
+class Mario 
+  attr_reader :column,
+              :row 
+
+  def initialize(board)
+    @board = board
+    @column = column_placement
+    @row = row_placement
+  end
+
+  def column_placement
+    @board.bot_placement[0]
+  end 
+
+  def row_placement
+    @board.bot_placement[1]
+  end
+end 
+
+class Princess
+  attr_reader :column,
+              :row
+
+  def initialize(board)
+    @board = board
+    @column = column_placement
+    @row = row_placement
+  end
+
+  def column_placement
+    @board.princess_placement[0]
+  end 
+
+  def row_placement
+    @board.princess_placement[1]
+  end
 end 
 
 class SavePrincess
   attr_reader :board,
-              :bot_location,
-              :princess_location,
+              :mario,
+              :princess,
               :row_difference,
               :column_difference
 
   def initialize(board_size, board)
     @board = Board.new(board_size, board)
-    @bot_location = @board.bot_placement
-    @princess_location = @board.princess_placement
+    @princess = Princess.new(@board)
+    @mario = Mario.new(@board)
     @row_difference = row_difference
     @column_difference = column_difference
   end
 
   def row_difference 
-    row_distance = @bot_location[1] - @princess_location[1]
+    row_distance = @mario.row_placement - @princess.row_placement
   end
 
   def column_difference
-    column_distance = @bot_location[0] - @princess_location[0] 
+    column_distance = @mario.column_placement - @princess.column 
   end
 
   def path_to_princess
